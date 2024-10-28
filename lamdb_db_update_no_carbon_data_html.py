edebba74fa4952bba2773cc9a19315747da3e9cd
velo789 <164281485+velo789@users.noreply.github.com>
2024-10-28 19:03:01 +0900

    Add files via upload

---
+++ lamdb_db_update_no_carbon_data_html.py
@@ -0,0 +1,89 @@
+import boto3
+import csv
+import os
+import logging
+from boto3.dynamodb.conditions import Attr
+from decimal import Decimal
+
+# 로깅 설정
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    # DynamoDB 및 S3 클라이언트 초기화
+    dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-2')  # 서울 리전
+    s3 = boto3.client('s3')
+    table_name = 'Policies'
+    table = dynamodb.Table(table_name)
+    
+    # S3 이벤트에서 버킷 이름과 파일 키 가져오기
+    for record in event['Records']:
+
+        if record['eventName'].startswith('ObjectCreated:'):  # ObjectCreated 이벤트만 처리
+            bucket_name = record['s3']['bucket']['name']
+            object_key = record['s3']['object']['key']
+            
+            # 특정 파일 이름 검사
+            if 'updated_policies_with_years_and_country_id.csv' in object_key:
+                logger.info(f"Processing file: {object_key} from bucket: {bucket_name}")
+                
+                # S3에서 CSV 파일 다운로드 경로 설정
+                download_path = '/tmp/Policies1.csv'
+                try:
+                    s3.download_file(bucket_name, object_key, download_path)
+                    logger.info(f"Downloaded {object_key} to {download_path}")
+                except Exception as e:
+                    logger.error(f"Error downloading file {object_key} from bucket {bucket_name}: {e}")
+                    continue
+                
+                # CSV 파일에서 데이터를 읽어 DynamoDB에 삽입
+                try:
+                    with open(download_path, newline='', encoding='utf-8') as csvfile:
+                        reader = csv.DictReader(csvfile, quotechar='"', quoting=csv.QUOTE_ALL)
+                        for row in reader:
+                            try:
+                                # Year 필드를 정수형으로 변환
+                                year = int(row['Year']) if row['Year'] else None
+                                
+                                # Implementation Cost를 정수형으로 변환
+                                implementation_cost = int(Decimal(row['Implementation Cost'])) if row['Implementation Cost'] else None
+                                
+                                # Estimated Carbon Reduction (%)와 (Million Tons)를 Decimal로 변환
+                                estimated_carbon_reduction = Decimal(row['Estimated Carbon Reduction (%)']) if row['Estimated Carbon Reduction (%)'] else None
+                                estimated_carbon_reduction_mt = Decimal(row['Estimated Carbon Reduction (Million Tons)']) if row['Estimated Carbon Reduction (Million Tons)'] else None
+                                
+                                table.put_item(
+                                    Item={
+                                        'Policy_id': row['Policy_id'],  # CSV에서 직접 Policy_id 사용
+                                        'Policy': row['Policy'],  # 필드명 수정
+                                        'Policy_URL': row['Policy URL'],  # 필드명 수정
+                                        'Year': year,  # 정수형으로 변환된 Year 필드
+                                        'Jurisdiction': row['Jurisdiction'],
+                                        'Content': row['Content'],
+                                        'Topics': row['Topics'],
+                                        'Policy_Types': row['Policy Types'],  # 필드명 수정
+                                        'Sectors': row['Sectors'],
+                                        'Implementation_cost': implementation_cost,  # 정수형으로 변환된 필드
+                                        'Duration': int(row['Duration']) if row['Duration'] else None,
+                                        'Estimated_carbon_reduction': estimated_carbon_reduction,  # % 필드
+                                        'Estimated_carbon_reduction_mt': estimated_carbon_reduction_mt,  # Million Tons 필드
+                                        'Country_id': row['Country_id'],
+                                        'Country': row['Country'],
+                                    },
+                                    ConditionExpression=Attr('Policy_id').not_exists()  # 중복 방지 조건
+                                )
+                                logger.info(f"Inserted item with Policy_id: {row['Policy_id']}")
+                            except dynamodb.meta.client.exceptions.ConditionalCheckFailedException:
+                                # 아이템이 이미 존재할 경우, 아무 작업도 하지 않음
+                                logger.warning(f"Item with Policy_id {row['Policy_id']} already exists.")
+                            except Exception as e:
+                                logger.error(f"Error inserting item with Policy_id {row['Policy_id']}: {e}")
+                except Exception as e:
+                    logger.error(f"Error reading CSV file {download_path}: {e}")
+            else:
+                logger.info(f"Ignored file: {object_key}")
+    
+    return {
+        'statusCode': 200,
+        'body': f'Data loaded into table {table_name} from {object_key}.'
+    }
---
+++ main_train_2.py
@@ -0,0 +1,240 @@
+import pandas as pd
+import csv
+
+def load_and_clean_population_data(filepath):
+    population_data = pd.read_csv(filepath)
+    if 'Unnamed: 0' in population_data.columns:
+        population_data = population_data.drop(columns=['Unnamed: 0'])
+    if '0' in population_data.columns:
+        population_data = population_data.drop(columns=['0'])
+    unique_countries = population_data['Country Name'].unique()
+    new_country_mapping = {country: i + 1 for i, country in enumerate(unique_countries)}
+    population_data['Country_id'] = population_data['Country Name'].map(new_country_mapping)
+    population_data['Country_id'] = population_data['Country_id'].astype(int)
+    return population_data
+
+def melt_population_data(population_data):
+    population_melted = pd.melt(population_data, id_vars=['Country Name', 'Country Code', 'Country_id'],
+                                value_vars=population_data.columns[4:],
+                                var_name='year', value_name='Population_total')
+    population_sorted = population_melted.sort_values(by=['Country_id', 'year'])
+    return population_sorted
+
+def save_data(data, filepath):
+    data.to_csv(filepath, index=False, quoting=csv.QUOTE_NONNUMERIC, quotechar='"')
+
+def load_and_clean_gdp_data(filepath, value_name):
+    gdp_data = pd.read_csv(filepath)
+    gdp_data_melted = pd.melt(gdp_data, id_vars=['Country Name', 'Country Code', 'Indicator Name'],
+                              value_vars=gdp_data.columns[4:],
+                              var_name='year', value_name=value_name)
+    gdp_sorted = gdp_data_melted.sort_values(by=['Country Name', 'Country Code', 'year'])
+    return gdp_sorted
+
+def load_and_clean_emissions_data(filepath):
+    data = pd.read_csv(filepath)
+    data_filtered = data[data['sector'] != 'LULUCF']
+    data_sorted = data_filtered.sort_values(by=['region', 'year'])
+    return data_sorted
+
+def map_region_data(region_data, population_data):
+    population_data_unique = population_data.groupby('Country Name').first().reset_index()
+    region_mapping = population_data_unique.set_index('Country Code')[['Country Name', 'Country_id']].to_dict(orient='index')
+
+    def map_region(row):
+        region_key = 'EUU' if row['region'] == 'EU27' else row['region']
+        if region_key in region_mapping:
+            return pd.Series([region_mapping[region_key]['Country Name'], region_key, region_mapping[region_key]['Country_id']])
+        else:
+            return pd.Series([None, row['region'], None])
+
+    region_data[['Country Name', 'Country Code', 'Country_id']] = region_data.apply(map_region, axis=1)
+    cols = ['Country Name', 'Country Code', 'Country_id'] + [col for col in region_data.columns if col not in ['Country Name', 'Country Code', 'Country_id', 'region']]
+    region_data = region_data[cols]
+    region_data.rename(columns={'value': 'carbon_emissions_value (MtCO2e GWP-AR4)'}, inplace=True)
+    if 'unit' in region_data.columns:
+        region_data = region_data.drop(columns=['unit'])
+    return region_data
+
+def get_eu27_value(row, data, column_name, eu27_country_ids, euu_country_id):
+    country_id = row['Country_id']
+    if country_id in eu27_country_ids:
+        country_id = euu_country_id
+    value = data[(data['Country_id'] == country_id) & (data['year'] == row['year'])]
+    if not value.empty:
+        return value[column_name].tolist()[0] if len(value[column_name].tolist()) > 0 else None
+    else:
+        return None
+
+def expand_rows_with_multiple_values(data, column_names):
+    expanded_data = []
+    for idx, row in data.iterrows():
+        max_len = max([len(row[col]) if isinstance(row[col], list) else 1 for col in column_names])
+        for i in range(max_len):
+            new_row = row.copy()
+            for col in column_names:
+                if isinstance(row[col], list):
+                    new_row[col] = row[col][i] if i < len(row[col]) else None
+            expanded_data.append(new_row)
+    return pd.DataFrame(expanded_data)
+
+def load_and_clean_policies(filepath, population_data):
+    policies_data = pd.read_csv(filepath)
+    country_name_mapping = {
+        'Plurinational State of Bolivia': 'Bolivia',
+        'Bolivarian Republic of Venezuela': 'Venezuela, RB',
+        'Republic of Moldova': 'Moldova',
+        'Saint Vincent and the Grenadines': 'St. Vincent and the Grenadines',
+        "Cote D'ivoire": "Cote d'Ivoire",
+        'Czech Republic': 'Czechia',
+        'Vatican City': 'Holy See',
+        'Saint Lucia': 'St. Lucia',
+        'United Republic of Tanzania': 'Tanzania',
+        'Micronesia (Federated States of)': 'Micronesia, Fed. Sts.',
+        "People's Republic of China": 'China',
+        'Chinese Taipei': 'Taiwan',
+        'Islamic Republic of Iran': 'Iran, Islamic Rep.',
+        "Lao People's Democratic Republic": 'Lao PDR',
+        'Kyrgyzstan': 'Kyrgyz Republic',
+        'Republic of the Congo': 'Congo, Rep.',
+        'Democratic Republic of the Congo': 'Congo, Dem. Rep.',
+        'Korea': 'Korea, Rep.',
+        'Egypt': 'Egypt, Arab Rep.',
+        'Yemen': 'Yemen, Rep.'
+    }
+    reverse_mapping = population_data.set_index('Country Name')['Country_id'].to_dict()
+    policies_data['Country'] = policies_data['Country'].replace(country_name_mapping)
+    policies_data['Country_id'] = policies_data['Country'].map(reverse_mapping)
+    current_max_id = max(reverse_mapping.values()) if reverse_mapping else 0
+    for index, row in policies_data[policies_data['Country_id'].isna()].iterrows():
+        if row['Country'] not in reverse_mapping:
+            current_max_id += 1
+            reverse_mapping[row['Country']] = current_max_id
+            policies_data.at[index, 'Country_id'] = current_max_id
+        else:
+            policies_data.at[index, 'Country_id'] = reverse_mapping[row['Country']]
+    policies_data['Country_id'] = policies_data['Country_id'].astype(int)
+    years_to_fill = list(range(1948, 1930, -1))
+    year_index = 0
+    for index, row in policies_data.iterrows():
+        if pd.isnull(row['Year']) or row['Year'] == 'N/A':
+            if year_index < len(years_to_fill):
+                policies_data.at[index, 'Year'] = years_to_fill[year_index]
+                year_index += 1
+            else:
+                break
+    policies_data['Year'] = pd.to_numeric(policies_data['Year'], errors='coerce').astype('Int64')
+    policies_data['Policy_id'] = policies_data['Country_id'].astype(str) + "_" + policies_data['Policy']
+    policies_data.fillna('N/A', inplace=True)
+    if 'Status' in policies_data.columns:
+        policies_data.drop(columns=['Status'], inplace=True)
+    return policies_data
+
+def main():
+
+    # Population data processing
+    population_data = load_and_clean_population_data('Cleaned_Population_Data.csv')
+    population_sorted = melt_population_data(population_data)
+    save_data(population_sorted, 'sorted_population_data.csv')
+
+    # GDP data processing (previous growth)
+    gdp_previous_sorted = load_and_clean_gdp_data('Cleaned_gdp_data1.csv', 'previous_GDP_growth_percent')
+    save_data(gdp_previous_sorted, 'sorted_gdp_growth_data_previous.csv')
+
+    # GDP data processing (current USD)
+    gdp_current_sorted = load_and_clean_gdp_data('cleaned_gdp_data_2.csv', 'GDP_current_USD')
+    save_data(gdp_current_sorted, 'sorted_gdp_current_usd.csv')
+
+    # Emissions data processing
+    emissions_data_sorted = load_and_clean_emissions_data('08032024_CountryAssessmentData_no_capita-fixed_2.csv')
+    save_data(emissions_data_sorted, 'sorted_data_without_LULUCF.csv')
+
+    # Emissions data processing step 1
+    region_data = pd.read_csv('sorted_data_without_LULUCF.csv')
+    region_data = map_region_data(region_data, population_data)
+    save_data(region_data, 'updated_region_data.csv')
+
+    # Emissions data processing step 2
+    population_data = pd.read_csv('sorted_population_data.csv')
+    gdp_data_current = pd.read_csv('sorted_gdp_current_usd.csv')
+    gdp_data_previous = pd.read_csv('sorted_gdp_growth_data_previous.csv')
+    carbon_data = pd.read_csv('updated_region_data.csv')
+
+    # 새로운 GDP 데이터 정제 코드 추가
+    # CSV 파일 로드
+    population_data = pd.read_csv('sorted_population_data.csv')
+    gdp_data_current = pd.read_csv('sorted_gdp_current_usd.csv')
+    gdp_data_previous = pd.read_csv('sorted_gdp_growth_data_previous.csv')
+
+    # 중복되지 않는 Country ID를 가져오기 위한 함수
+    def assign_country_id(row, country_id_mapping):
+        if row['Country Name'] in country_id_mapping:
+            return country_id_mapping[row['Country Name']]
+        return None
+
+    # Population 데이터에서 Country Name과 Country ID를 매핑
+    country_id_mapping = population_data.set_index('Country Name')['Country_id'].to_dict()
+
+    # GDP 데이터에 Country ID 추가
+    gdp_data_current['Country_id'] = gdp_data_current.apply(assign_country_id, axis=1, country_id_mapping=country_id_mapping)
+    gdp_data_previous['Country_id'] = gdp_data_previous.apply(assign_country_id, axis=1, country_id_mapping=country_id_mapping)
+
+    # Country ID를 정수형으로 변환
+    gdp_data_current['Country_id'] = gdp_data_current['Country_id'].astype(int, errors='ignore')
+    gdp_data_previous['Country_id'] = gdp_data_previous['Country_id'].astype(int, errors='ignore')
+
+    # 열 이름에 단위 추가
+    gdp_data_current.rename(columns={'GDP_current_USD': 'GDP_current_USD (current US$)'}, inplace=True)
+    gdp_data_previous.rename(columns={'previous_GDP_growth_percent': 'GDP_growth_percent (annual %)'}, inplace=True)
+
+    # 'Indicator Name' 열을 삭제
+    gdp_data_current.drop(columns=['Indicator Name'], inplace=True)
+    gdp_data_previous.drop(columns=['Indicator Name'], inplace=True)
+
+    # 업데이트된 데이터를 CSV로 저장, quoting과 quotechar 옵션 추가
+    gdp_data_current.to_csv('updated_gdp_data_current_with_units.csv', index=False, quoting=csv.QUOTE_NONNUMERIC, quotechar='"')
+    gdp_data_previous.to_csv('updated_gdp_data_previous_with_units.csv', index=False, quoting=csv.QUOTE_NONNUMERIC, quotechar='"')
+
+    # Emissions data processing
+    emissions_data_sorted = load_and_clean_emissions_data('08032024_CountryAssessmentData_no_capita-fixed_2.csv')
+    save_data(emissions_data_sorted, 'sorted_data_without_LULUCF.csv')
+
+    # Emissions data processing step 1
+    region_data = pd.read_csv('sorted_data_without_LULUCF.csv')
+    region_data = map_region_data(region_data, population_data)
+    save_data(region_data, 'updated_region_data.csv')
+
+    # Emissions data processing step 2
+    population_data = pd.read_csv('sorted_population_data.csv')
+    gdp_data_current = pd.read_csv('updated_gdp_data_current_with_units.csv')
+    gdp_data_previous = pd.read_csv('updated_gdp_data_previous_with_units.csv')
+    carbon_data = pd.read_csv('updated_region_data.csv')
+
+    eu27_country_ids = [15, 18, 22, 54, 55, 59, 71, 72, 76, 79, 90, 100, 102, 111, 116, 143, 144, 145, 159, 176, 194, 200, 221, 222]
+    euu_country_id = population_data.loc[population_data['Country Code'] == 'EUU', 'Country_id'].values[0]
+
+    population_data['GDP_current_USD (current US$)'] = population_data.apply(get_eu27_value, axis=1, data=gdp_data_current, column_name='GDP_current_USD (current US$)', eu27_country_ids=eu27_country_ids, euu_country_id=euu_country_id)
+    population_data['GDP_growth_percent (annual %)'] = population_data.apply(get_eu27_value, axis=1, data=gdp_data_previous, column_name='GDP_growth_percent (annual %)', eu27_country_ids=eu27_country_ids, euu_country_id=euu_country_id)
+    population_data['carbon_emissions_value (MtCO2e GWP-AR4)'] = population_data.apply(get_eu27_value, axis=1, data=carbon_data, column_name='carbon_emissions_value (MtCO2e GWP-AR4)', eu27_country_ids=eu27_country_ids, euu_country_id=euu_country_id)
+
+    column_names_to_expand = ['GDP_current_USD (current US$)', 'GDP_growth_percent (annual %)', 'carbon_emissions_value (MtCO2e GWP-AR4)']
+    population_data_expanded = expand_rows_with_multiple_values(population_data, column_names_to_expand)
+
+    mask = population_data_expanded['carbon_emissions_value (MtCO2e GWP-AR4)'].notnull()
+    population_data_expanded.loc[mask, 'GDP_current_USD (current US$)'] = population_data_expanded.loc[mask, 'GDP_current_USD (current US$)'].fillna(method='ffill')
+    population_data_expanded.loc[mask, 'GDP_growth_percent (annual %)'] = population_data_expanded.loc[mask, 'GDP_growth_percent (annual %)'].fillna(method='ffill')
+    population_data_expanded.rename(columns={'year': 'Year'}, inplace=True)
+ 
+    data_cleaned = population_data_expanded.dropna(subset=['Population_total', 'GDP_current_USD (current US$)',
+                                                           'GDP_growth_percent (annual %)', 'carbon_emissions_value (MtCO2e GWP-AR4)'])
+    min_year = data_cleaned['Year'].min()
+    max_year = data_cleaned['Year'].max()
+    data_filtered = data_cleaned[(data_cleaned['Year'] >= min_year) & (data_cleaned['Year'] <= max_year)]
+    save_data(data_filtered, 'final_combined_data_filtered.csv')
+
+    # Policies data processing
+    policies_data = load_and_clean_policies('new_policy.csv', population_data)
+    save_data(policies_data, 'updated_policies_with_years_and_country_id.csv')
+
+if __name__ == "__main__":
+    main()
